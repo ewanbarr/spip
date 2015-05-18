82f1ee83eb168edb2751d89a78ce2cde0fafd46e
Andrew Jameson <ajameson@swin.edu.au>
2015-05-18 15:06:41 +1000

    shifted main to function

--- scripts/spip_recv.py
+++ scripts/spip_recv.py
@@ -9,6 +9,7 @@
 
 import os, threading, sys, time, socket, select, signal, traceback
 import spip
+import spip_smrb
 
 SCRIPT = "spip_recv"
 DL     = 2
@@ -19,75 +20,99 @@ def signal_handler(signal, frame):
   quit_event.set()
 
 #################################################################
-#
 # main
-#
+def main (argv):
 
-# this should come from command line argument
-id = 0
+  # this should come from command line argument
+  stream_id = argv[1]
 
-# read configuration file
-cfg = spip.getConfig()
+  # read configuration file
+  cfg = spip.getConfig()
 
-control_thread = []
+  control_thread = []
 
-log_file  = cfg["SERVER_LOG_DIR"] + "/" + SCRIPT + ".log"
-pid_file  = cfg["SERVER_CONTROL_DIR"] + "/" + SCRIPT + ".pid"
-quit_file = cfg["SERVER_CONTROL_DIR"] + "/"  + SCRIPT + ".quit"
+  log_file  = cfg["SERVER_LOG_DIR"] + "/" + SCRIPT + ".log"
+  pid_file  = cfg["SERVER_CONTROL_DIR"] + "/" + SCRIPT + ".pid"
+  quit_file = cfg["SERVER_CONTROL_DIR"] + "/"  + SCRIPT + ".quit"
 
-if os.path.exists(quit_file):
-  sys.stderr.write("quit file existed at launch: " + quit_file)
-  sys.exit(1)
+  if os.path.exists(quit_file):
+    sys.stderr.write("quit file existed at launch: " + quit_file)
+    sys.exit(1)
 
-# become a daemon
-spip.daemonize(pid_file, log_file)
+  # become a daemon
+  spip.daemonize(pid_file, log_file)
 
-try:
+  try:
 
-  spip.logMsg(1, DL, "STARTING SCRIPT")
+    spip.logMsg(1, DL, "STARTING SCRIPT")
 
-  quit_event = threading.Event()
+    quit_event = threading.Event()
 
-  signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGINT, signal_handler)
 
-  # start a control thread to handle quit requests
-  control_thread = spip.controlThread(quit_file, pid_file, quit_event, DL)
-  control_thread.start()
+    # start a control thread to handle quit requests
+    control_thread = spip.controlThread(quit_file, pid_file, quit_event, DL)
+    control_thread.start()
 
-  log_host = cfg["LOG_HOST"]
-  log_port = cfg["LOG_PORT"]
+    log_host = cfg["LOG_HOST"]
+    log_port = cfg["LOG_PORT"]
 
-  log_pipe = spip.openSocket (DL, log_host, log_port, 3)
+    log_pipe = spip.openSocket (DL, log_host, log_port, 3)
 
-  cmd = cfg["PWC_BINARY"] \
-        + " -b " + cfg["RECV_CORE_" + id] \
-        + " -c " + cfg["RECV_PORT_" + id] \
-        + " -i " + cfg["RECV_UDP_IP_" + id]
-        + " -l " + cfg["RECV_LOGPORT_" + id] \
-        + " -p " + cfg["RECV_UDP_PORT_" + id]
+    db_id = cfg["PROCESSING_DB_ID"]
+    db_prefix = cfg["DATA_BLOCK_PREFIX"]
+    num_stream = cfg["NUM_STREAM"]
+    db_key = spip_smrb.getDBKey (db_prefix, stream_id, num_stream, db_id)
+    spip.logMsg(0, DL, "db_key="+db_key)
 
-  spip.logMsg(2, DL, "main: " + command)
-  rval = spip.system_piped (command, pipe, 2 < DL)
-  spip.logMsg(2, DL, "main: rval=" + str(rval))
+    # wait up to 10s for the SMRB to be created
+    smrb_wait = 10
+    cmd = "dada_dbmetric -k " + db_key
+    rval = 1
+    while rval && smrb_wait > 0:
+      rval, lines = spip.system(cmd)
+      if rval:
+        time.sleep(1)
+      smrb_wait--
 
-  # now we want to start a popen command that pipes stderr and stdout 
-  # to a redirectable socket
+    if rval:
+      spip.logMsg(-2, DL, "spip_smrb["+str(stream_id)+"] no valid SMRB with " +
+                  "key=" + db_key)
+      quit_event.set()
 
-  # script body
-  time.sleep (2)
+    else:
 
-except:
-  spip.logMsg(-2, DL, "main: exception caught: " + str(sys.exc_info()[0]))
-  print '-'*60
-  traceback.print_exc(file=sys.stdout)
-  print '-'*60
-  quit_event.set()
+      cmd = cfg["PWC_BINARY"] \
+            + " -b " + cfg["RECV_CORE_" + id] \
+            + " -c " + cfg["RECV_PORT_" + id] \
+            + " -k " + db_key \
+            + " -i " + cfg["RECV_UDP_IP_" + id] \
+            + " -l " + cfg["RECV_LOGPORT_" + id] \
+            + " -p " + cfg["RECV_UDP_PORT_" + id]
 
-# join threads
-spip.logMsg(2, DL, "main: joining control thread")
-if (control_thread):
-  control_thread.join()
+      # this should be a persistent / blocking command 
+      spip.logMsg(2, DL, "main: " + command)
+      rval = spip.system_piped (command, pipe, 2 < DL)
+      spip.logMsg(2, DL, "main: rval=" + str(rval))
 
-spip.logMsg(1, DL, "STOPPING SCRIPT")
+  except:
+    spip.logMsg(-2, DL, "main: exception caught: " + str(sys.exc_info()[0]))
+    print '-'*60
+    traceback.print_exc(file=sys.stdout)
+    print '-'*60
+    quit_event.set()
 
-sys.exit(0)
+  # join threads
+  spip.logMsg(2, DL, "main: joining control thread")
+  if (control_thread):
+    control_thread.join()
+
+  spip.logMsg(1, DL, "STOPPING SCRIPT")
+
+if __name__ == "__main__":
+  if len(sys.argv) != 2:
+    print "ERROR: 1 command line argument expected"
+    sys.exit(1)
+
+  main (sys.argv)
+  sys.exit(0)
