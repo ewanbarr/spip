2412c1a65884eb338f61455a8ded135a548ceffb
Andrew Jameson <andrew.jameson@gmail.com>
2016-09-27 11:52:28 +0200

    Added psrcat utilities

---
+++ scripts/spip/utils/catalog.py
@@ -0,0 +1,33 @@
+###############################################################################
+#  
+#     Copyright (C) 2016 by Andrew Jameson
+#     Licensed under the Academic Free License version 2.1
+# 
+###############################################################################
+
+from spip.utils.core import system
+
+# test whether the specified target exists in the pulsar catalog
+def test_pulsar_valid (target):
+
+  (reply, message) = get_psrcat_param (target, "name")
+  if reply != "ok":
+    return (reply, message)
+
+  if message == target:
+    return ("ok", "")
+  else:
+    return ("fail", "pulsar " + target + " did not exist in catalog")
+
+def get_psrcat_param (target, param):
+  cmd = "psrcat -all " + target + " -c " + param + " -nohead -o short"
+  rval, lines = system (cmd)
+  if rval != 0 or len(lines) <= 0:
+    return ("fail", "could not use psrcat")
+
+  if lines[0].startswith("WARNING"):
+    return ("fail", "pulsar " + target_name + " did not exist in catalog")
+
+  parts = lines[0].split()
+  if len(parts) == 2 and parts[0] == "1":
+    return ("ok", parts[1])
